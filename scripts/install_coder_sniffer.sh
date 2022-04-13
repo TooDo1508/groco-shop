cdc313357e9426cfebe21067fcaf7ef1407c19df
manh.nguyen <manh.nguyen@ffwagency.com>
2022-04-13 14:11:27 +0700

    Update script

--- scripts/install_coder_sniffer.sh
+++ scripts/install_coder_sniffer.sh
@@ -1,5 +1,5 @@
 echo -e "Installing Coder Sniffer...\n"
 composer global require drupal/coder:8.2.12
-composer global require squizlabs/php_codesniffer:2.9.1
 echo 'export PATH=$HOME/.composer/vendor/bin:$PATH' >> $BASH_ENV && source $BASH_ENV
+composer require dealerdirect/phpcodesniffer-composer-installer
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
