264069052ddf3c5df20015f1698705526a778559
manh.nguyen <manh.nguyen@ffwagency.com>
2022-04-13 14:40:59 +0700

    Fix phpcs

--- scripts/install_coder_sniffer.sh
+++ scripts/install_coder_sniffer.sh
@@ -1,5 +1,4 @@
 sudo composer self-update --1
 composer global require drupal/coder:8.2.12
-composer global require squizlabs/php_codesniffer:2.9.1
 echo 'export PATH=$HOME/.composer/vendor/bin:$PATH' >> $BASH_ENV && source $BASH_ENV
 phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
--- web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -1,42 +1,52 @@
 <?php
-    // create form from Acquia
 
-    namespace Drupal\rsvplist\Form;
+// Create form from Acquia.
+namespace Drupal\rsvplist\Form;
 
-    use Drupal\Core\Database\Database;
-    use Drupal\Core\Form\FormBase;
-    use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
 
+/**
+ *
+ */
+class RSVPForm extends FormBase {
 
-    class RSVPForm extends FormBase{
+  /**
+   *
+   */
+  public function getFormId() {
+    return 'rsvplist_email_form';
+  }
 
-        public function getFormId(){
-            return 'rsvplist_email_form';
-        }
+  /**
+   *
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $node = \Drupall::routeMatch()->getParamter('node');
+    $nid = $node->nid->value;
+    $form['email'] = [
+      '#title' => t('Email Address'),
+      '#type' => 'textfield',
+      '#size' => 25,
+      '#description' => t("We'll send update to ur email"),
+      '#require' => TRUE,
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => t('RSVP'),
+    ];
+    $form['nid'] = [
+      '#type' => 'hidden',
+      '#value' => $nid,
+    ];
+    return $form;
+  }
 
-        public function buildForm(array $form, FormStateInterface $form_state){
-            $node= \Drupall::routeMatch()->getParamter('node');
-            $nid = $node->nid->value;
-            $form['email'] = array(
-                '#title' => t('Email Address'),
-                '#type' => 'textfield',
-                '#size' => 25,
-                '#description' => t("We'll send update to ur email"),
-                '#require' => TRUE,
-            );
-            $form['submit'] = array(
-                '#type' => 'submit',
-                '#value' => t('RSVP'),
-            );
-            $form['nid'] => array(
-                '#type' => 'hidden',
-                '#value' => $nid,
-            );
-            return $form; 
-        }
+  /**
+   *
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    drupal_set_message(t('The form is working!'));
+  }
 
-        public function submitForm (array &$form, FormStateInterface $form_state){
-            drupal_set_message(t('The form is working!'));
-        }
-
-    }
+}
